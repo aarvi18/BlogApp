7f534dbec4d75f8995648ebedb1616c58ebf6693
Rakesh <rakeshcse123@gmail.com>
2024-02-26 16:56:39 +0530

    add store

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { Header, Footer } from "./components/index";
+import { login, logout } from "./store/authSlice";
+// import { Outlet } from "react-router-dom";
 
 const App = () => {
-  return (
-    <div>Welcome to blog app</div>
-  )
-}
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-export default App
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        todo
+        <main>{/* <Outlet /> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
+};
+
+export default App;
